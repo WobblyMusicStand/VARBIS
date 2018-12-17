725d7a0dbf4e217047da8fcf07bb7aa4e4afcaa1
Anders Grasdal <t5q0b@ugrad.cs.ubc.ca>
2018-12-16 20:49:15 -0800

    Initial Commit

---
+++ VARBIS_Main/VARBIS_Main.ino
@@ -0,0 +1,16 @@
+// VARBIS main program sketch for the Arduino MKR1000 and MPU-6050
+// Adapted from the MPU6050_DMP6 and RUBS_8plus_Summer sketches.
+// using the I2C device class (12Cdev), MPU6050 class, and DMP (MotionApps v2.0)
+// 2018-11-01
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
